0cf8d4b3c8f4702c05bbb6329ae86fc990863d03
rishijivani <rishijivani28@gmail.cim>
2024-10-22 00:53:52 +0400

    Your commit message

--- Pawsome/CatPost.swift
+++ Pawsome/CatPost.swift
@@ -2,24 +2,23 @@ import Foundation
 import SwiftData
 
 @Model
-public class CatPost {
-    // Define the properties of the model
-    var id: UUID
-    var username: String
-    var name: String
-    var breed: String
-    var age: String
-    var location: String
-    var postDescription: String
-    var imageData: Data?
-    var likes: Int
-    var comments: [String]
-    var creationDate: Date
-    var modificationDate: Date
+public class CatPost: Identifiable {
+    public var id: UUID
+    public var username: String
+    public var name: String
+    public var breed: String
+    public var age: String
+    public var location: String
+    public var postDescription: String
+    public var imageData: Data?
+    public var likes: Int
+    public var comments: [String]
+    public var creationDate: Date
+    public var modificationDate: Date
 
     // Initializer for the model
-    init(username: String, name: String, breed: String, age: String, location: String, postDescription: String, imageData: Data?) {
-        self.id = UUID()
+    public init(username: String, name: String, breed: String, age: String, location: String, postDescription: String, imageData: Data?) {
+        self.id = UUID() // Now public
         self.username = username
         self.name = name
         self.breed = breed
