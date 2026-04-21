59b8392415b6cd91d53c77ed3a05bda356ff7d8d
baldbuffalo <rishijivani28@gmail.com>
2026-04-21 23:14:58 +0400

    Update Extensions

--- Pawsome/Extensions.swift
+++ Pawsome/Extensions.swift
@@ -5,7 +5,7 @@ extension Timestamp {
     /// Returns a human-readable "X ago" string.
     func timeAgoDisplay() -> String {
         let diff = Calendar.current.dateComponents(
-            [.year, .month, .weekOfYear, .day, .hour, .minute],
+            [.year, .month, .weekOfYear, .day, .hour, .minute, .second],
             from: dateValue(), to: Date()
         )
         if let y  = diff.year,       y  >= 1 { return "\(y)y ago" }
@@ -14,6 +14,7 @@ extension Timestamp {
         if let d  = diff.day,        d  >= 1 { return "\(d)d ago" }
         if let h  = diff.hour,       h  >= 1 { return "\(h)h ago" }
         if let m  = diff.minute,     m  >= 1 { return "\(m)m ago" }
+        if let s  = diff.second,     s  >= 1 { return "\(s)s ago" }
         return "just now"
     }
 }
