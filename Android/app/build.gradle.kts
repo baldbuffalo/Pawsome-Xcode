358488f75e8b41c3f04478848a5c518f2ac78dde
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-08-26 04:10:37 +0400

    Fix Android Firebase package ID match

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 37
 
     defaultConfig {
-        applicationId = "com.GameBuster.Pawsome"
+        applicationId = "Com.GameBuster.Pawsome"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
