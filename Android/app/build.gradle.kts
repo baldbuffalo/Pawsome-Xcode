d84a4c724cd7ba8235e8a55a8d4a03d7c598ba16
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-08-26 14:20:27 +0400

    Update core-ktx to 1.19.0

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -46,7 +46,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.19.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.11.0")
     implementation("androidx.activity:activity-compose:1.13.0")
     implementation(platform("androidx.compose:compose-bom:2026.08.00"))
