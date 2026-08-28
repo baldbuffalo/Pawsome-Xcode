ff9216ba578df53ad1e046cd27c9f911256e0750
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-08-28 23:46:33 +0400

    Add coroutine Play Services support for Firestore SDK

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -79,6 +79,7 @@ dependencies {
     implementation("androidx.credentials:credentials-play-services-auth:1.6.0")
     implementation("com.google.android.libraries.identity.googleid:googleid:1.2.0")
     implementation("com.google.android.gms:play-services-auth:21.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.11.0")
     implementation("io.coil-kt.coil3:coil-compose:3.5.0")
     implementation("io.coil-kt.coil3:coil-network-okhttp:3.5.0")
     implementation("com.squareup.okhttp3:okhttp:5.5.0")
