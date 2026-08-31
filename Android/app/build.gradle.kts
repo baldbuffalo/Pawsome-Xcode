3ffdc17fe3a3a2ff7b338138424f51113ba81849
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-08-31 04:11:45 +0400

    Fix deprecated Firebase source directory configuration

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
     // Keep Android Firebase implementations in the top-level Firebase folder.
     sourceSets {
         getByName("main") {
-            java.srcDir("../../Firebase/Android")
+            directories.add("../../Firebase/Android")
         }
     }
 
